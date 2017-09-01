6ae73636a317bbc5a44e5fe67cea74890f0be9b9
boopathi4545 <31505777+boopathi4545@users.noreply.github.com>
2017-09-01 08:58:27 -0700

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("%d enter the number");
+cin>>a;
+if(a>0)
+printf("%d the number is positive");
+elesif(a<0)
+printf("%d the number is negative");
+eles
+printf("%d the number is zero");
+}
